bce0a91c2ae6ac9a08667e0d29f5b1cce4c7e62b
jonathan de catelle <72255006+JoNaTHaN2303@users.noreply.github.com>
2022-12-07 20:45:33 +0100

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,7 @@ export default function Product({naam, info, foto, url}){
             <img className='h-80' src={foto} alt=""/>
         </div>
         <p className="text-xl lg:text-2xl border-b-2 mt-2 pb-1 border-gray-400">{naam}</p>
-        <p className='h-16 text-base lg:text-lg mt-2 pb-1'>{info}</p>
+        <p className='text-base lg:text-lg mt-2 pb-1'>{info}</p>
         <a className='flex justify-center items-center rounded-md w-full h-10 mt-2 shadow-xl hover:bg-gray-400 hover:text-[#373A36] duration-500' href={url} target="_blank" rel='noreferrer'>Link naar de productpagina</a>
     </div>
   )
