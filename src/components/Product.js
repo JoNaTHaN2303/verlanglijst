6d81d61b5b329544ad762011ab15b5cddf8b3da0
jonathan de catelle <72255006+JoNaTHaN2303@users.noreply.github.com>
2023-11-13 00:12:40 +0100

    Update Product.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,11 +7,11 @@ export default function Product({naam, info, foto, url}){
   return(
     <div className="">
         <div className="bg-white flex justify-center">
-            <img className='h-80' src={images[foto]} alt=""/>
+            <img className='h-80 rounded-sm border-2 border-gray-300' src={images[foto]} alt=""/>
         </div>
-        <p className="text-xl lg:text-2xl border-b-2 mt-2 pb-1 border-gray-400">{naam}</p>
+        <a className='flex justify-center items-center rounded-md w-full h-10 mt-2 shadow-xl bg-gray-300 text-[#373A36] hover:bg-gray-400 duration-500' href={url} target="_blank" rel='noreferrer'>Link naar product</a>
+        <p className="text-xl lg:text-2xl border-b-2 mt-2 pb-1 border-gray-300">{naam}</p>
         <p className='text-base lg:text-lg mt-2 pb-1'>{info}</p>
-        <a className='flex justify-center items-center rounded-md w-full h-10 mt-2 shadow-xl bg-gray-400 text-[#373A36] hover:bg-gray-500 duration-500' href={url} target="_blank" rel='noreferrer'>Link naar de productpagina</a>
     </div>
   )
 }
