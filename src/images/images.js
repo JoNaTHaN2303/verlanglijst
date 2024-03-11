c59a645f528c838a8bee7f695e91f59a7858fca1
jonathan de catelle <72255006+JoNaTHaN2303@users.noreply.github.com>
2024-03-11 15:20:13 +0100

    update verjaardag 2024

--- src/images/images.js
+++ src/images/images.js
@@ -2,30 +2,22 @@ import Biertoren from "./Biertoren.jpg"
 import Ijsjesblokken from "./Ijsjesblokken.jpg"
 import Keukenweegschaal from "./Keukenweegschaal.jpg"
 import Keukenwekker from "./Keukenwekker.jpg"
-import Kookboek from "./Kookboek.jpg"
-import Lunchbox from "./LunchBox.webp"
 import Monitorlamp from "./Monitorlamp.jpg"
-import Onderzetters from "./Onderzetters.jpg"
-import Rietjes from "./Rietjes.jpg"
 import Schaakspel from "./Schaakspel.jpg"
-import WCGolf from "./WCGolf.jpg"
-import XboxBatterij from "./XboxBatterij.jpg"
 import XboxController from "./XboxController.jpg"
 import Zonnebril from "./Zonnebril.webp"
+import IpadHoesje from "./IpadHoesje.jpg"
+import Drankdispenser from "./Drankdispenser.webp"
 
 export{
-    Lunchbox,
     Zonnebril,
     Biertoren,
     Monitorlamp,
     Keukenwekker,
     XboxController,
-    XboxBatterij,
-    Rietjes,
-    WCGolf,
     Ijsjesblokken,
-    Kookboek,
     Schaakspel,
-    Onderzetters,
-    Keukenweegschaal
+    Keukenweegschaal,
+    IpadHoesje,
+    Drankdispenser
 }
